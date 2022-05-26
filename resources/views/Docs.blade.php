64b0da65006a07b3660b28efa742745a1e77d19d
mrbeastmahmoud <mahmoudsu22@gmail.com>
2022-05-26 18:14:59 +0300

    front end fixes

--- resources/views/Docs.blade.php
+++ resources/views/Docs.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Docs</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="/resources/css/app.css">
