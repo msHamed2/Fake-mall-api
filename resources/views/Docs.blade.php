3b2bf09c78099567d847f8df9c62ddd06d9abf7f
mrbeastmahmoud <mahmoudsu22@gmail.com>
2022-05-26 18:19:34 +0300

    tab icon change

--- resources/views/Docs.blade.php
+++ resources/views/Docs.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="/resources/css/app.css">
-
+    <link rel="icon" href="{!! asset('assets/logo.png') !!}"/>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
