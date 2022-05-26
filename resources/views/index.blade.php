8db820812b475d27ca8deac59f843503e5f005c7
mrbeastmahmoud <mahmoudsu22@gmail.com>
2022-05-26 18:20:38 +0300

    clean up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,17 +13,7 @@
 
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160318377-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-
-        gtag('config', 'UA-160318377-2');
-    </script>
     <style>
         .alert {
             width: 100%;
